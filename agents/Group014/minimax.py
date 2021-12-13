1313df5e20b6548e14e0f631cb869d15f27cb7fd
Arron Fox <arron.fox@student.manchester.ac.uk>
2021-12-13 16:21:19 +0000

    update minimax

--- agents/Group014/minimax.py
+++ agents/Group014/minimax.py
@@ -3,7 +3,6 @@ from dijkstra import *
 from helper_functions import *
 from random import choice
 
-
 def minimax(board, depth, alpha, beta, maximising, last_move):
     """
     Applies that minimax algorithm with alpha-beta pruning to the current state of
@@ -24,48 +23,46 @@ def minimax(board, depth, alpha, beta, maximising, last_move):
     if depth == 0:
         return get_static_evaluation(board)
 
+    legal_moves = get_free_nodes(board)
     next_move = "B" if last_move == "R" else "R"
     if maximising:
         current_max = float("-inf")
-        for row in board:
-            for node in row:
-                x, y = node.coordinates
-                new_board = deepcopy(board)
-                new_board[x][y].occupy(next_move)
-                current_evaluation = minimax(
-                    new_board,
-                    depth - 1,
-                    alpha,
-                    beta,
-                    False,
-                    next_move,
-                )
-                current_max = max(current_max, current_evaluation)
-                alpha = max(alpha, current_evaluation)
-                if beta <= alpha:
-                    break
+        for node in legal_moves:
+            x, y = node.coordinates
+            new_board = deepcopy(board)
+            new_board[x][y].occupy(next_move)
+            current_evaluation = minimax(
+                new_board,
+                depth - 1,
+                alpha,
+                beta,
+                False,
+                next_move,
+            )
+            current_max = max(current_max, current_evaluation)
+            alpha = max(alpha, current_evaluation)
+            if beta <= alpha:
+                break
         return current_max
     else:
         current_min = float("inf")
-        for row in board:
-            for node in row:
-                x, y = node.coordinates
-                new_board = deepcopy(board)
-                new_board[x][y].occupy(next_move)
-                current_evaluation = minimax(
-                    new_board,
-                    depth - 1,
-                    alpha,
-                    beta,
-                    True,
-                    next_move,
-                )
-                current_min = min(current_min, current_evaluation)
-                beta = min(beta, current_evaluation)
-                if beta <= alpha:
-                    break
+        for node in legal_moves:
+            x, y = node.coordinates
+            new_board = deepcopy(board)
+            new_board[x][y].occupy(next_move)
+            current_evaluation = minimax(
+                new_board,
+                depth - 1,
+                alpha,
+                beta,
+                True,
+                next_move,
+            )
+            current_min = min(current_min, current_evaluation)
+            beta = min(beta, current_evaluation)
+            if beta <= alpha:
+                break
         return current_min
-
 
 def get_minimax_depth_level(perc):
     """
@@ -81,54 +78,57 @@ def get_minimax_depth_level(perc):
     else:
         return 1
 
-
 def get_static_evaluation(board):
-    """
-    Calculates and returns the heuristic (the difference between the length of the
-    Dijkstra path of the blue player with the length of the Dijkstra path of the red
-    player).
 
-    The path for red is chosen at random based on the free or red occupied nodes on the
-    top and bottom row, while blue uses the same logic for left and right.
 
-    :param board: The current game board.
-    :returns: The difference between the two paths.
-    """
+
     free_nodes = get_free_nodes(board)
     last_node_index = len(board[0]) - 1
 
-    src_red = []
-    for node in board[0]:
-        if node.is_free or node.colour == "R":
-            src_red.append(node)
-    src_red = choice(src_red)
+    # src_red = [node for node in board[0] if node.is_free]
+    # dest_red = [node for node in board[last_node_index] if node.is_free]
 
-    dest_red = []
-    # If the node directly downwards from the source is free, choose it.
-    if board[last_node_index][src_red.coordinates[1]].is_free:
-        dest_red.append(board[last_node_index][src_red.coordinates[1]])
-    else:
-        for node in board[last_node_index]:
-            if node.is_free or node.colour == "R":
-                dest_red.append(node)
-    dest_red = choice(dest_red)
+    src_red = board[0][last_node_index]
+    dest_red = board[last_node_index][0]
 
-    src_blue = []
-    for row in board:
-        if row[0].is_free or row[0].colour == "B":
-            src_blue.append(row[0])
-    src_blue = choice(src_blue)
+    src_blue = board[1][0]
+    dest_blue = board[1][last_node_index]
 
-    dest_blue = []
-    # If the node directly across from the source is free, choose it.
-    if board[src_blue.coordinates[0]][last_node_index].is_free:
-        dest_blue.append(board[src_blue.coordinates[0]][last_node_index])
-    else:
-        for row in board:
-            if row[last_node_index].is_free or row[last_node_index].colour == "B":
-                dest_blue.append(row[last_node_index])
-    dest_blue = choice(dest_blue)
+    # src_blue = []
+    # dest_blue = []
+    # for x in board:
+    #     if x[0].is_free:
+    #         src_blue.append(x[0])
+    #     if x[last_node_index].is_free:
+    #         dest_blue.append(x[last_node_index])
+    #
+    # if len(src_red) == 0 or len(dest_red) == 0 or len(src_blue) == 0 or len(dest_blue) == 0:
+    #     return 0
 
-    return len(dijkstra(free_nodes, src_blue, dest_blue)) - len(
-        dijkstra(free_nodes, src_red, dest_red)
-    )
+    #  DEBUG
+    # dijkstra_result_red = dijkstra(free_nodes, src_red, dest_red)
+    # coordinates_r = [red_node.coordinates for red_node in dijkstra_result_red]
+    # print("red:", coordinates_r)
+    # dijkstra_result_blue = dijkstra(free_nodes, src_blue, dest_blue)
+    # coordinates_b = [blue_node.coordinates for blue_node in dijkstra_result_blue]
+    # print("blue:", coordinates_b)
+
+    # dijkstra_result = len(dijkstra_result_blue) - len(dijkstra_result_red)
+    # print("final result:", dijkstra_result)
+    # dijkstra_result = len(dijkstra(free_nodes, choice(src_blue), choice(dest_blue))) - len(dijkstra(free_nodes, choice(src_red), choice(dest_red)))
+
+    coordinates = []
+    for x in range(len(board)):
+        coordinates.append([])
+        for y in range(len(board[0])):
+            coordinates[x].append(distance_between_points((len(board) // 2, len(board[0]) // 2), (x,y)))
+            # coordinates[x].append([0, 9 - distance_between_points((len(board) // 2, len(board[0]) // 2))])
+            # elf.board[x].append([0, 9 - self.distance_between_points((self.board_size // 2, self.board_size // 2), (x, y))])
+
+    print(coordinates)
+    red_count = 0
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j].colour == 'R':
+                red_count += coordinates[i][j]
+    return red_count
